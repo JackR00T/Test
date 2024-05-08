a69f0688ed48ee0d5a18579ccfd457382b681ecd
jackr00t <jackr00t@jackr00tdeMacBook-Pro.local>
2024-05-08 11:50:43 +0800

    dev:sc_jenkins:测试

---
+++ src/main/java/org/example/Executor.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Executor {
+    private String executeCommand(String command) throws IOException, InterruptedException {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("sh", "-c", command);
+
+        Process process = processBuilder.start();
+        InputStream inputStream = process.getInputStream();
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        StringBuilder output = new StringBuilder();
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            output.append(line);
+            output.append(System.lineSeparator());
+        }
+
+        int exitCode = process.waitFor();
+        if (exitCode != 0) {
+            throw new IOException("Command failed with exit code " + exitCode);
+        }
+
+        return output.toString();
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,20 @@
 package org.example;
 
+
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        Executor executor = new Executor();
+        try {
+            String output = executor.executeCommand(" mvn dependency:tree -DoutputFile=${PWD}/mavenDependency.txt -DoutputType=text -B");
+            System.out.println(output);
+        }catch (IOException|InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
